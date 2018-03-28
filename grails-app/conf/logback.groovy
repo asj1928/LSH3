cfa747198e3c040d4dbe9cd5fcf5fc79f34df076
ajay <ajay.singh@tothenew.com>
2018-03-28 16:33:54 +0530

    com1

--- grails-app/conf/logback.groovy
+++ grails-app/conf/logback.groovy
@@ -12,13 +12,15 @@ conversionRule 'wex', WhitespaceThrowableProxyConverter
 appender('STDOUT', ConsoleAppender) {
     encoder(PatternLayoutEncoder) {
         charset = Charset.forName('UTF-8')
+        pattern ="%d{MM/dd HH:mm:ss} %-5p %c{12} - %m%n "
 
-        pattern =
-                '%clr(%d{yyyy-MM-dd HH:mm:ss.SSS}){faint} ' + // Date
-                        '%clr(%5p) ' + // Log level
-                        '%clr(---){faint} %clr([%15.15t]){faint} ' + // Thread
-                        '%clr(%-40.40logger{39}){cyan} %clr(:){faint} ' + // Logger
-                        '%m%n%wex' // Message
+
+//        pattern =
+//                '%clr(%d{yyyy-MM-dd HH:mm:ss.SSS}){faint} ' + // Date
+//                        '%clr(%5p) ' + // Log level
+//                        '%clr(---){faint} %clr([%15.15t]){faint} ' + // Thread
+//                        '%clr(%-40.40logger{39}){cyan} %clr(:){faint} ' + // Logger
+//                        '%m%n%wex' // Message
     }
 }
 
@@ -31,6 +33,11 @@ if (Environment.isDevelopmentMode() && targetDir != null) {
             pattern = "%level %logger - %msg%n"
         }
     }
-    logger("StackTrace", ERROR, ['FULL_STACKTRACE'], false)
+//    logger("StackTrace", ERROR, ['FULL_STACKTRACE'], false)
 }
-root(ERROR, ['STDOUT'])
+//root(ERROR, ['STDOUT'])
+logger "LSH3Changed",INFO,['STDOUT'],false
+root(INFO, ['STDOUT'])
+
+logger "LSH3Changed",DEBUG,['STDOUT'],false
+root(DEBUG, ['STDOUT'])
